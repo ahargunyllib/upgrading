6b5f37003f4f512f739af7b2ee99c3b248a7f49b
ahargunyllib <billy.bpm03@gmail.com>
2024-01-10 00:30:31 +0700

    style: changing check box theme

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
-  return ThemeData( 
+  return ThemeData(
     primaryColor: const Color(0xFF00416A),
-  
     textTheme: const TextTheme(
-      bodyLarge: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold, color: Colors.black),
-      bodyMedium: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.black),
+      bodyLarge: TextStyle(
+          fontSize: 22.0, fontWeight: FontWeight.bold, color: Colors.black),
+      bodyMedium: TextStyle(
+          fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.black),
       bodySmall: TextStyle(fontSize: 14.0, color: Colors.black),
       displayLarge: TextStyle(fontSize: 12.0, color: Colors.grey),
       displayMedium: TextStyle(fontSize: 12.0, color: Colors.grey),
@@ -20,32 +21,30 @@ ThemeData appTheme() {
       titleLarge: TextStyle(fontSize: 12.0, color: Colors.grey),
       titleMedium: TextStyle(fontSize: 12.0, color: Colors.grey),
       titleSmall: TextStyle(fontSize: 12.0, color: Colors.grey),
-
     ),
-
     buttonTheme: ButtonThemeData(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       buttonColor: const Color(0xFF00416A),
       textTheme: ButtonTextTheme.primary,
     ),
-
     inputDecorationTheme: InputDecorationTheme(
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(8),
-        borderSide: const BorderSide(color: Colors.grey),
+        borderSide: const BorderSide(color: Color(0xFFAAC0CD)),
       ),
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(8),
         borderSide: const BorderSide(color: Color(0xFF00416A)),
       ),
     ),
-
-    // Checkbox theme
     checkboxTheme: CheckboxThemeData(
-      fillColor: MaterialStateProperty.all(const Color(0xFF00416A)),
+      fillColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return const Color(0xFF00416A);
+        }
+        return Colors.white;
+      }),
     ),
-
-    // Elevated button theme
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: const Color(0xFF00416A),
