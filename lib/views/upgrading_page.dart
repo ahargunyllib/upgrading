c081eba1424808d38ef2c74a7c12326157e3b68d
ahargunyllib <billy.bpm03@gmail.com>
2024-01-12 00:59:26 +0700

    fix: changing navigation logic

--- lib/views/upgrading_page.dart
+++ lib/views/upgrading_page.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'konsultasi_page.dart';
+import 'video_persiapan_page.dart';
+
 class UpgradingPage extends StatelessWidget {
+  static const routeName = "/upgrading";
   const UpgradingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      onGenerateRoute: (RouteSettings settings) {
+        WidgetBuilder builder;
+        switch (settings.name) {
+          case '/':
+            builder = (BuildContext context) => const UpgradingMainWidget();
+            break;
+          case VideoPersiapanPage.routeName:
+            builder = (BuildContext context) => const VideoPersiapanPage();
+            break;
+          case KonsultasiPage.routeName:
+            builder = (BuildContext context) => const KonsultasiPage();
+            break;
+          default:
+            throw Exception('Invalid route: ${settings.name}');
+        }
+        return MaterialPageRoute(builder: builder, settings: settings);
+      },
+    );
+  }
+}
+
+class UpgradingMainWidget extends StatelessWidget {
+  const UpgradingMainWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,20 +46,23 @@ class UpgradingPage extends StatelessWidget {
             const Text("Akses Fitur Premium"),
             const SizedBox(height: 33),
             ListTile(
-              leading:
-                  Image.asset("assets/images/video-preparation-icon.png"),
-              title: const Text("Video Preparation"),
+              leading: Image.asset("assets/images/video-preparation-icon.png"),
+              title: const Text("Video Persiapan"),
               subtitle: const Text(
                   "Akses konten video berisi persiapan beasiswa atau organisasi yang Anda inginkan"),
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).pushNamed(VideoPersiapanPage.routeName);
+              },
             ),
             const SizedBox(height: 24),
             ListTile(
               leading: Image.asset("assets/images/consultation-icon.png"),
-              title: const Text("Consultation"),
+              title: const Text("Konsultasi"),
               subtitle: const Text(
                   "Konsultasi dengan penerima beasiswa atau anggota organisasi yang Anda inginkan"),
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).pushNamed(KonsultasiPage.routeName);
+              },
             )
           ],
         ),
