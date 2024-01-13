9ce538ae7206300e6e4e18025429a3c057f3827b
ahargunyllib <billy.bpm03@gmail.com>
2024-01-13 16:11:09 +0700

    style: add center

--- lib/views/upgrading/video_persiapan/video_persiapan_page.dart
+++ lib/views/upgrading/video_persiapan/video_persiapan_page.dart
@@ -66,138 +66,140 @@ class VideoPersiapanMainWidget extends StatelessWidget {
             ],
           ),
         ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Card(
-                  color: theme.primaryColor,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        top: 16, bottom: 24, left: 16, right: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset("assets/images/video-preparation-icon.png"),
-                        const SizedBox(height: 8),
-                        Text("Akses Fitur Premium",
-                            style: GoogleFonts.poppins(
-                                color: Colors.white,
-                                fontSize: 24,
-                                fontWeight: FontWeight.w700)),
-                        Text("Video Persiapan",
-                            style: GoogleFonts.poppins(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500)),
-                        const SizedBox(height: 12),
-                        Text("Paket sudah termasuk:",
-                            style: GoogleFonts.poppins(
-                                color: Colors.white,
-                                fontSize: 10,
-                                fontWeight: FontWeight.w400)),
-                        const SizedBox(height: 28),
-                        Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(Icons.check_circle,
-                                  color: Colors.white),
-                              const SizedBox(width: 8),
-                              Text("Akses konten video penulisan esai",
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.white,
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w400)),
-                            ]),
-                        const SizedBox(height: 10),
-                        Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(Icons.check_circle,
-                                  color: Colors.white),
-                              const SizedBox(width: 8),
-                              Text("Akses konten video penulisan esai",
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.white,
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w400)),
-                            ]),
-                        const SizedBox(height: 10),
-                        Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(Icons.check_circle,
-                                  color: Colors.white),
-                              const SizedBox(width: 8),
-                              Text("Akses konten video penulisan esai",
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.white,
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w400)),
-                            ]),
-                        const SizedBox(height: 10),
-                        Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(Icons.check_circle,
-                                  color: Colors.white),
-                              const SizedBox(width: 8),
-                              Text("Akses konten video penulisan esai",
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.white,
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w400)),
-                            ]),
-                        const SizedBox(height: 24),
-                        SizedBox(
-                          width: double.infinity,
-                          height: 38,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.pushNamed(
-                                  context, PremiumPaymentPage.routeName);
-                            },
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8)),
-                              backgroundColor: Colors.white,
-                            ),
-                            child: Text(
-                              "Rp. xxx.xxx.xx-",
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                    color: theme.primaryColor,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 16, bottom: 24, left: 16, right: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/images/video-preparation-icon.png"),
+                          const SizedBox(height: 8),
+                          Text("Akses Fitur Premium",
                               style: GoogleFonts.poppins(
-                                color: theme.primaryColor,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w700)),
+                          Text("Video Persiapan",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500)),
+                          const SizedBox(height: 12),
+                          Text("Paket sudah termasuk:",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w400)),
+                          const SizedBox(height: 28),
+                          Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.check_circle,
+                                    color: Colors.white),
+                                const SizedBox(width: 8),
+                                Text("Akses konten video penulisan esai",
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.white,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w400)),
+                              ]),
+                          const SizedBox(height: 10),
+                          Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.check_circle,
+                                    color: Colors.white),
+                                const SizedBox(width: 8),
+                                Text("Akses konten video penulisan esai",
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.white,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w400)),
+                              ]),
+                          const SizedBox(height: 10),
+                          Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.check_circle,
+                                    color: Colors.white),
+                                const SizedBox(width: 8),
+                                Text("Akses konten video penulisan esai",
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.white,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w400)),
+                              ]),
+                          const SizedBox(height: 10),
+                          Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.check_circle,
+                                    color: Colors.white),
+                                const SizedBox(width: 8),
+                                Text("Akses konten video penulisan esai",
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.white,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w400)),
+                              ]),
+                          const SizedBox(height: 24),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 38,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                    context, PremiumPaymentPage.routeName);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                backgroundColor: Colors.white,
+                              ),
+                              child: Text(
+                                "Rp. xxx.xxx.xx-",
+                                style: GoogleFonts.poppins(
+                                  color: theme.primaryColor,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700,
+                                ),
                               ),
                             ),
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      ),
+                    )),
+                const SizedBox(height: 16),
+                GestureDetector(
+                  child: Text(
+                    "Kembali",
+                    style: GoogleFonts.poppins(
+                      color: theme.primaryColor,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
                     ),
-                  )),
-              const SizedBox(height: 16),
-              GestureDetector(
-                child: Text(
-                  "Kembali",
-                  style: GoogleFonts.poppins(
-                    color: theme.primaryColor,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
                   ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
